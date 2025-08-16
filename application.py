cc045b2803e72c2fd0aed95515b40dfd2827cccd
Saida Yengui <saida.yengui@gmail.com>
2025-08-16 10:50:37 +0100

    update application

---
+++ application.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from recommender import recommend_movies
+
+st.title("🎬 Movie Recommender")
+title = st.selectbox("Pick a movie", df["title"])
+if st.button("Recommend"):
+    recommendations = recommend_movies(title)
+    st.write(recommendations) 
